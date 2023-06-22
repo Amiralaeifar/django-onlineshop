855619f49c35c8ee1e039169d9516212b5818a7a
amiralaeifar <amplusir@gmail.com>
2023-06-22 12:03:29 +0330

    custom user admin and fields, accoutns.admin

--- Ecommerce/accounts/admin.py
+++ Ecommerce/accounts/admin.py
@@ -1,3 +1,30 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.models import Group
+from .forms import UserChangeForm, UserCreationFrom
+from .models import User 
 
-# Register your models here.
+
+class UserAdmin(BaseUserAdmin):
+    form = UserChangeForm
+    add_form = UserCreationFrom
+    
+    list_display = ('full_name', 'email', 'phone_number', 'is_admin')
+    list_filter = ('is_admin',)
+    
+    fieldsets = (
+        (None, {'fields': ('full_name', 'email', 'phone_number', 'password')}),
+        ('permissions', {'fields': ('is_admin', 'is_active', 'last_login')})
+    )
+    
+    add_fieldsets = (
+        (None, {'fields': ('full_name', 'email', 'phone_number', 'password1', 'password2')}),
+    )
+    
+    search_fields = ('full_name', 'email')
+    ordering = ('full_name',)
+    filter_horizontal = ()
+    
+    
+admin.site.unregister(Group)
+admin.site.register(User, UserAdmin)
