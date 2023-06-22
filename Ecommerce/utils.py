174f9a7e98c707024352b4197df7cbde2656611b
amiralaeifar <amplusir@gmail.com>
2023-06-22 17:39:13 +0330

    sendign verification code via kavenegar API

--- Ecommerce/utils.py
+++ Ecommerce/utils.py
@@ -1,4 +1,19 @@
-
+from kavenegar import *
 
 def send_otp_code(phone_number, code):
-    pass
+    try:
+        api = KavenegarAPI('Your api key')
+        params = {
+            'sender': '',
+            'receptor': phone_number,
+            'message': f'Your verification code is {code}'
+        }
+        response = api.sms_send(params)
+        
+    except APIException as e:
+        print(e.decode('utf-8'))
+        
+    except HTTPException as e:
+        print(e.decode('utf-8'))
+    
+    
