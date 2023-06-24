bc7840e9c15b7355a512179d655e2d42694554e1
amiralaeifar <amplusir@gmail.com>
2023-06-24 03:47:44 +0330

    product and categories model added

--- Ecommerce/home/admin.py
+++ Ecommerce/home/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Category, Product
 
-# Register your models here.
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'slug')
+    search_fields = ('name',)
+    
+    
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'category', 'price', 'available')
+    search_fields = ('name', 'category')
+    
