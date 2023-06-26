e0b561995096dd50c715fd7aa3866aba11b92a98
amiralaeifar <amplusir@gmail.com>
2023-06-26 07:33:21 +0330

    bugs on bucket.py

---
+++ Ecommerce/bucket.py
@@ -0,0 +1,35 @@
+import boto3
+from django.conf import settings
+import logging
+from botocore.exceptions import ClientError
+
+
+class Bucket:
+    
+    def __init__(self):
+        #logging.basicConfig(level=logging.INFO)
+        self.s3_resource = boto3.resource(
+            's3',
+            endpoint_url = settings.AWS_S3_ENDPOINT_URL,
+            aws_access_key_id = settings.AWS_ACCESS_KEY_ID,
+            aws_secret_access_key = settings.AWS_SECRET_ACCESS_KEY,
+        )
+            
+        """   
+        try:
+            bucket_name = settings.AWS_STORAGE_BUCKET_NAME
+            bucket = s3_resource.Bucket(bucket_name)
+            bucket.create(ACL='public-read')
+            
+        except ClientError as exc:
+            logging.error(exc)
+            """
+    
+    def get_objects(self):
+        for bucket in self.s3_resource.buckets.all():
+            return bucket
+        
+        
+        
+bucket = Bucket()
+        
