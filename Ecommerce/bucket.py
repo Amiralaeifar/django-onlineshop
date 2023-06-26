f6f0b089fdd5b2b91cbb878764bfbf325b1030b1
amiralaeifar <amplusir@gmail.com>
2023-06-26 07:35:11 +0330

    bucket bug fixed

--- Ecommerce/bucket.py
+++ Ecommerce/bucket.py
@@ -8,16 +8,12 @@ class Bucket:
     
     def __init__(self):
         #logging.basicConfig(level=logging.INFO)
-        try:
-            s3_resource = boto3.resource(
-                's3',
-                endpoint_url = settings.AWS_S3_ENDPOINT_URL,
-                aws_access_key_id = settings.AWS_ACCESS_KEY_ID,
-                aws_secret_access_key = settings.AWS_SECRET_ACCESS_KEY,
-            )
-        except Exception as exc:
-            logging.info(exc)
-            
+        self.s3_resource = boto3.resource(
+            's3',
+            endpoint_url = settings.AWS_S3_ENDPOINT_URL,
+            aws_access_key_id = settings.AWS_ACCESS_KEY_ID,
+            aws_secret_access_key = settings.AWS_SECRET_ACCESS_KEY,
+        )
         """   
         try:
             bucket_name = settings.AWS_STORAGE_BUCKET_NAME
